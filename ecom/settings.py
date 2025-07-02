cc10d90603fdad03d0d28d40cb4088487672a11c
Erra Anggi <erranggit@gmail.com>
2025-07-02 21:57:09 +0900

    added gunicorn

--- ecom/settings.py
+++ ecom/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': os.environ.get['DB_PASSWORD'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
     }
