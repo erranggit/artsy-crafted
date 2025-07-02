73a49a1e5c3c8c7e28459a1da17c1f0311f5b3d3
Erra Anggi <erranggit@gmail.com>
2025-07-02 21:23:16 +0900

    added allowed hosts for railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -21,8 +21,10 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['web-production-9634.up.railway.app',
+                 'https://web-production-9634.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['web-production-9634.up.railway.app',
+                        'https://web-production-9634.up.railway.app']
 
 
 # Application definition
